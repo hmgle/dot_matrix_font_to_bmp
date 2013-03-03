0a34a6734af0b434b5566726f0db948a187eb4aa
mingang.he <dustgle@gmail.com>
2013-03-03 16:00:17 +0800

    增加MIT授权协议

---
+++ charactersv2bmp.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 要utf-8方式编码的输入才能正常运行
+# cygwin的运行环境需要设置 Options->Text->Character set:选择utf-8
+# 以垂直方向合并位图
+
+help()
+{
+	cat <<EOF
+
+	USAGE: $0 [-h] "要生成位图的字符" > 位图存放路径
+
+	EXAMPLES:
+	$0 "汉字" > ./output.bmp
+EOF
+}
+
+if { [ -z "$1" ] && [ -t 0 ] ; } || [ "$1" == '-h' ]
+then
+	help
+	exit 0
+fi
+echo "$1"|./utf8togb2312 |./gb2312tobmps |./bmpsallv2bmp
---
+++ testpoem.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+(./characters2bmp.sh "红豆生南国，" \
+&& ./characters2bmp.sh "春来发几枝？" \
+&& ./characters2bmp.sh "愿君多采撷，" \
+&& ./characters2bmp.sh "此物最想思。") \
+| ./bmpsallv2bmp > poem.bmp
