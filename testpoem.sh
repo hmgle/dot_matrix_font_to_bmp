8003230f73098d0c7e073dc8b5306a81eedb076c
mingang.he <dustgle@gmail.com>
2013-03-03 20:26:28 +0800

    testpoem.sh is modified

--- testpoem.sh
+++ testpoem.sh
@@ -3,5 +3,7 @@
 (./characters2bmp.sh "红豆生南国，" \
 && ./characters2bmp.sh "春来发几枝？" \
 && ./characters2bmp.sh "愿君多采撷，" \
-&& ./characters2bmp.sh "此物最想思。") \
+&& ./characters2bmp.sh "此物最想思。" \
+&& ./characters2bmp.sh "      " \
+&& ./characters2bmp.sh "  -王维 ") \
 | ./bmpsallv2bmp > poem.bmp
